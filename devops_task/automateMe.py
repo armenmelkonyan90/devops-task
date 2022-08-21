6d656dfdadaa8f547fb07d9c88e77e18ae7239ef
hakobmkoyan771 <95763847+hakobmkoyan771@users.noreply.github.com>
2022-08-22 03:46:50 +0400

    Update automateMe.py

--- devops_task/automateMe.py
+++ devops_task/automateMe.py
@@ -1,150 +1,67 @@
 #!/usr/bin/python3
-from xmlrpc.client import boolean
 from python_terraform import *
-import argparse
 import os
 import json
-import sys
+import base64
+import git
+import yaml
 
-nfs_acr = Terraform(working_dir="./instances")
-#nfs_acr = Terraform(working_dir="./nfs-acr")
-#aks = Terraform(working_dir="./AKS")
+git.Git(".").clone("https://github.com/hakobmkoyan771/infra.git")
 
-def check_nfs_acr_tfstate():
-    print("Checking if there is available nfs ecr")
-    nfs_acr_tfstate = open('./instances/terraform.tfstate')
-    nfs_acr_data = json.load(nfs_acr_tfstate)
-    nfs_acr_resources = len(nfs_acr_data['resources'])
-    nfs_acr_tfstate.close()
+nfs_acr = Terraform(working_dir="./nfs-acr")
+aks = Terraform(working_dir="./AKS")
 
-#    nfs_acr_tfstate = open('./nfs-acr/terraform.tfstate')
-#    nfs_acr_data = json.load(nfs_acr_tfstate)
-#    nfs_acr_resources = len(nfs_acr_data['resources'])
-#    nfs_acr_tfstate.close()
+def change_pv_file(addr):
+    with open('infra/Volumes/pv.yaml') as pvManifest:
+        data = yaml.load(pvManifest, Loader=yaml.FullLoader)
+        data['spec']['nfs']['server'] = str(addr)
+        with open('infra/Volumes/pv.yaml', 'w') as newManifest:
+            yaml.dump(data, newManifest)
 
-    if nfs_acr_resources == 0:
-        return False
-    return True
+def change_secret_file(cred):
+    with open('infra/Secrets/webapps-reg-secret.yaml') as secretManifest:
+        data = yaml.load(secretManifest, Loader=yaml.FullLoader)
+        data['data']['.dockerconfigjson'] = str(cred)
+        with open('infra/Secrets/webapps-reg-secret.yaml', 'w') as newManifest:
+            yaml.dump(data, newManifest)
 
-def initialize(init_nfs_acr, init_aks):
-    if init_nfs_acr is True:
-        try:
-            print("initializing nfs acr")
-            nfs_acr.init()
-            print("nfs and acr initiallized")
-            return 0
-        except Exception as err:
-            print("NFS-ACR Init", err)
-            return 1   
+def encode_reg_config(acr_name_pswd):
+    dockerconfigjson = '''{"auths":{"https://index.docker.io/v1/":{"auth":"'''+str(acr_name_pswd)+'''"}}}'''
+    msg_bytes    = dockerconfigjson.encode('ascii')
+    base64_bytes = base64.b64encode(msg_bytes)
+    base64_msg   = base64_bytes.decode('ascii')
 
-    if init_aks is True:
-        try:
-            print("Initializing aks")
-            aks.init()
-            print("aks initialized")
-            return 0
-        except Exception as err:
-            print("AKS Init", err)
-            return 1        
+    return str(base64_msg)
 
-def plan(plan_nfs_acr, plan_aks):
-    if plan_nfs_acr is True:
-        try:
-            print(nfs_acr.plan())
-            return 0
-        except Exception as err:
-            print("NFS-ACR Plan", err)
-            return 1   
+def encode_acr_name_pswd(cred):
+    msg_bytes    = cred.encode('ascii')
+    base64_bytes = base64.b64encode(msg_bytes)
+    base64_msg   = base64_bytes.decode('ascii')
 
-    if plan_aks is True:
-        try:
-            print(aks.plan())
-            return 0
-        except Exception as err:
-            print("AKS Plan", err)
-            return 1        
+    return str(base64_msg)
 
-def apply(apply_nfs_acr, apply_aks):
-    if apply_nfs_acr is True and apply_aks is True:
-        try:
-            print("applying nfs-acr and aks")
-            nfs_acr.apply(skip_plan=True)
-            aks.apply(skip_plan=True)
-            print("nfs-acr and eks applied")
-            return 0
-        except Exception as err:
-            print("NFS-ACR, AKS Apply", err)
-            return 1     
-
-    if apply_nfs_acr is True:
-        try:
-            print("applying nfs acr")
-            nfs_acr.apply(skip_plan=True)
-            print("nfs acr applied")
-            return 0
-        except Exception as err:
-            print("NFS-ACR Apply", err)
-            return 1     
-
-    if apply_aks is True and apply_nfs_acr is False:
-        if check_nfs_acr_tfstate() is False:
-            print("Unable to create ACR without NFS & ACR")
-            sys.exit(1)
-        try:
-            print("applying aks")
-            aks.apply(skip_plan=True)
-            print("aks applied")
-            return 0
-        except Exception as err:
-            print("AKS Apply", err)
-            return 1                      
-    else:
-        try:
-            print("applying aks")
-            aks.apply(skip_plan=True)
-            print("aks applied")
-            return 0
-        except Exception as err:
-            print("AKS Apply", err)
-            return 1              
-
-def main(
-    init_nfs_acr,
-    init_aks,
-    plan_nfs_acr,
-    plan_aks,
-    apply_nfs_acr,
-    apply_aks,
-    ):
+def outs():
+    nfs_acr_out  = nfs_acr.output()
+    acr_username = str(nfs_acr_out['acr_username']['value'])
+    acr_password = str(nfs_acr_out['acr_password']['value'])
+    nfs_address  = str(nfs_acr_out['nfs_public_ip']['value'])
     
-    if init_nfs_acr is True or init_aks is True:
-        initialize(init_nfs_acr, init_aks)
+    return acr_username,acr_password,nfs_address
 
-    if plan_nfs_acr is True or plan_aks is True:
-        plan(plan_nfs_acr, plan_aks)        
+def main():
+    nfs_acr.apply(skip_plan=True)
+    outputs                = outs()
     
-    if apply_nfs_acr is True or apply_aks is True:
-        apply(apply_nfs_acr, apply_aks)
+    acrcred                = outputs[0]+":"+outputs[1]
+    nfsaddr                = outputs[2]
+    acr_encoded_cred       = encode_acr_name_pswd(acrcred)
+    acr_config_reg_encoded = encode_reg_config(acr_encoded_cred)
+    
+    change_secret_file(acr_config_reg_encoded)
+    change_pv_file(nfsaddr)
+
+    aks.apply(skip_plan=True)
+    
 
 if __name__ == "__main__":
-    arguments = argparse.ArgumentParser(description="Parameters for terraform codes")
-    
-    arguments.add_argument("--init-nfs-acr", type=bool, default=False, help="Terraform init nfs & acr")
-    arguments.add_argument("--init-aks", type=bool, default=False, help="Terraform init aks")
-                              
-    arguments.add_argument("--plan-nfs-acr", type=bool, default=False, help="Terraform plan nfs & acr")
-    arguments.add_argument("--plan-aks", type=bool, default=False, help="Terraform plan aks")
-                              
-    arguments.add_argument("--apply-nfs-acr", type=bool, default=False, help="Terraform apply nfs & acr")
-    arguments.add_argument("--apply-aks", type=bool, default=False, help="Terraform apply aks")  
-
-    args = arguments.parse_args()
-
-    main(
-        args.init_nfs_acr,
-        args.init_aks,
-        args.plan_nfs_acr,
-        args.plan_aks,
-        args.apply_nfs_acr,
-        args.apply_aks
-    )
+    main()
